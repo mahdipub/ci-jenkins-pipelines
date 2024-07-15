ae01f51d6907dbc76b9b0e1350f622eb0a889277
Mahdi Ardekanian <mahdi@ibm.com>
2024-07-15 11:32:56 -0400

    test digest with short image name when registery is non-default

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -2459,7 +2459,7 @@ class Build {
                             // Store the pulled docker image digest as 'buildinfo'
                             def long_docker_image_name = context.sh(script: "docker image ls | grep ${buildConfig.DOCKER_IMAGE} | head -n1 | awk '{print \$1}'", returnStdout:true).trim()
                             context.sh(script: "docker tag '${long_docker_image_name}' '${buildConfig.DOCKER_IMAGE}'", returnStdout:false)
-                            dockerImageDigest = context.sh(script: "docker inspect --format='{{.RepoDigests}}' ${long_docker_image_name}", returnStdout:true)
+                            dockerImageDigest = context.sh(script: "docker inspect --format='{{.RepoDigests}}' ${buildConfig.DOCKER_IMAGE}", returnStdout:true)
 
                             // Use our dockerfile if DOCKER_FILE is defined
                             if (buildConfig.DOCKER_FILE) {
